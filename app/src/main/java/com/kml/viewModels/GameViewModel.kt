ff3892a25940cccc19631cf284d47f4f10855e4e
Arek Mądry <arix20003@gmail.com>
2020-10-13 15:01:25 +0200

    commit before fragment convert to kotlin

---
+++ app/src/main/java/com/kml/viewModels/GameViewModel.kt
@@ -0,0 +1,14 @@
+package com.kml.viewModels
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.kml.data.internalRoomDatabase.GameDao
+import com.kml.data.models.Game
+import com.kml.repositories.GameRepository
+
+class GameViewModel(gameDao: GameDao) : ViewModel() {
+    private val repository: GameRepository = GameRepository()
+    val allGames: LiveData<List<Game>> = repository.allGames
+}
--- app/src/test/java/com/kml/KmlUnitTests.java
+++ app/src/test/java/com/kml/KmlUnitTests.java
@@ -1,13 +1,6 @@
 package com.kml;
 
-import android.content.Context;
-
-import com.kml.aGlobalUses.FileFactory;
-
-import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class KmlUnitTests
 {
