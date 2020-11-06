45755cadf7fb716a350a374f1eaa2f9af9411c5e
Arek Mądry <arix20003@gmail.com>
2020-11-06 18:26:46 +0100

    change implementation to @Parcelize

--- app/src/main/java/com/kml/data/models/GameFilterInfo.kt
+++ app/src/main/java/com/kml/data/models/GameFilterInfo.kt
@@ -1,41 +1,8 @@
 package com.kml.data.models
 
-import android.os.Parcel
 import android.os.Parcelable
-import java.lang.NullPointerException
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class GameFilterInfo(val name: String, val numberOfKids: String,
-                          val kidsAge: String, val place: String, val typeOfGame: String, val category: String) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "") {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeString(numberOfKids)
-        parcel.writeString(kidsAge)
-        parcel.writeString(place)
-        parcel.writeString(typeOfGame)
-        parcel.writeString(category)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<GameFilterInfo> {
-        override fun createFromParcel(parcel: Parcel): GameFilterInfo {
-            return GameFilterInfo(parcel)
-        }
-
-        override fun newArray(size: Int): Array<GameFilterInfo?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+                          val kidsAge: String, val place: String, val typeOfGame: String, val category: String) : Parcelable
--- app/src/main/java/com/kml/data/models/Volunteer.kt
+++ app/src/main/java/com/kml/data/models/Volunteer.kt
@@ -1,39 +1,11 @@
 package com.kml.data.models
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 class Volunteer(val id: Int,
                 @SerializedName("imie") val firstName: String,
                 @SerializedName("nazwisko") val lastName: String,
-                var isChecked: Boolean) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-            parcel.readInt(),
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readByte() != 0.toByte()) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(firstName)
-        parcel.writeString(lastName)
-        parcel.writeByte(if (isChecked) 1 else 0)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Volunteer> {
-        override fun createFromParcel(parcel: Parcel): Volunteer {
-            return Volunteer(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Volunteer?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+                var isChecked: Boolean) : Parcelable
