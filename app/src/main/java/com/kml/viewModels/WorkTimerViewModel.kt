dd4936afe1f951626829800eba70957a76b02948
Arek Mądry <arkadiusz.madry.2000@gmail.com>
2020-11-13 15:20:46 +0100

    improve MVVM

--- app/src/main/java/com/kml/viewModels/WorkTimerViewModel.kt
+++ app/src/main/java/com/kml/viewModels/WorkTimerViewModel.kt
@@ -1,11 +1,11 @@
 package com.kml.viewModels
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.kml.data.app.FileFactory
 import com.kml.data.models.Time
 import com.kml.data.models.WorkToAdd
+import com.kml.data.services.TimerService
 import com.kml.repositories.WorkTimerRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -32,35 +32,6 @@ class WorkTimerViewModel(fileFactory: FileFactory) : ViewModel() {
     var exitThread = false
     var isTimerRunning = false
 
-    fun setTime(): Time {
-        val secondsFormatted = formatSeconds(seconds)
-        if (seconds >= 60) {
-            minutes += 1
-            seconds = 0
-        }
-        val minutesFormatted = formatMinutes(minutes)
-
-        if (minutes >= 60) {
-            hours += 1
-            minutes = 0
-        }
-        val hoursFormatted = formatHours(hours)
-
-        return Time(hoursFormatted, minutesFormatted, secondsFormatted)
-    }
-
-    fun setTimeFromFile() {
-        val fromFile = repository.readFile()
-        val hms = fromFile.split(";".toRegex()).toTypedArray()
-        seconds = hms[0].toInt()
-        minutes = hms[1].toInt()
-        hours = hms[2].toInt()
-    }
-
-    fun saveToFile(toSave: String) {
-        repository.saveToFile(toSave)
-    }
-
     fun startCounting() {
         exitThread = false
         isTimerRunning = true
@@ -78,33 +49,21 @@ class WorkTimerViewModel(fileFactory: FileFactory) : ViewModel() {
         repository.clearFileState()
     }
 
-    fun isFileNotEmpty(): Boolean {
-        return repository.readFile().contains(";")
-    }
-
-    fun sendWorkToDatabase(work: WorkToAdd): Boolean {
-        return repository.addWorkToDatabase(work)
-    }
-
-    private val timer = { CoroutineScope(Dispatchers.Default).launch { countTime() } }
-
-    private fun countTime() {
-        if (isThreadAlive) {
-            return
+    fun setTime(): Time {
+        val secondsFormatted = formatSeconds(seconds)
+        if (seconds >= 60) {
+            minutes += 1
+            seconds = 0
         }
-        isThreadAlive = true
-        val timer = Timer()
-        timer.scheduleAtFixedRate(object : TimerTask() {
-            override fun run() {
-                if (exitThread) {
-                    cancel()
-                    isThreadAlive = false
-                } else {
-                    _seconds.postValue(seconds + 1)
-                    Log.d("VIEW_MODEL_TAG", "run: $seconds")
-                }
-            }
-        }, 0, 1000)
+        val minutesFormatted = formatMinutes(minutes)
+
+        if (minutes >= 60) {
+            hours += 1
+            minutes = 0
+        }
+        val hoursFormatted = formatHours(hours)
+
+        return Time(hoursFormatted, minutesFormatted, secondsFormatted)
     }
 
     private fun formatSeconds(seconds: Int): String =
@@ -127,7 +86,63 @@ class WorkTimerViewModel(fileFactory: FileFactory) : ViewModel() {
                 hours == 60 -> "00:"
                 else -> "$hours:"
             }
+
+    fun setTimeFromFile() {
+        val fromFile = repository.readFile()
+        val hms = fromFile.split(";".toRegex()).toTypedArray()
+        seconds = hms[0].toInt()
+        minutes = hms[1].toInt()
+        hours = hms[2].toInt()
+    }
+
+    fun keepCurrentTime()
+    {
+        if (!isTimerRunning && hours > 0 || minutes > 0 || seconds >= 10) {
+            saveToFile("$seconds;$minutes;$hours")
+        }
+    }
+
+    private fun saveToFile(toSave: String) {
+        repository.saveToFile(toSave)
+    }
+
+    fun isFileNotEmpty(): Boolean {
+        return repository.readFile().contains(";")
+    }
+
+    fun sendWorkToDatabase(work: WorkToAdd): Boolean {
+        return repository.addWorkToDatabase(work)
+    }
+
+    fun returnStateFromService() {
+        seconds = TimerService.seconds
+        minutes = TimerService.minutes
+        hours = TimerService.hours
+    }
+
+    fun saveStateToService() {
+        TimerService.seconds = seconds
+        TimerService.minutes = minutes
+        TimerService.hours = hours
+    }
+
+    private val timer = { CoroutineScope(Dispatchers.Default).launch { countTime() } }
+
+    private fun countTime() {
+        if (isThreadAlive)
+            return
+
+        isThreadAlive = true
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                if (exitThread) {
+                    cancel()
+                    isThreadAlive = false
+                } else {
+                    _seconds.postValue(seconds + 1)
+                }
+            }
+        }, 0, 1000)
+    }
 }
-
-
-
