cd5687d5d8c6a46b89d1c1323f1d49feeedf3d01
Arkadiusz Mądry <akadiusz.madry.2000@gmail.com>
2021-02-20 23:30:55 +0100

    refactor code

---
+++ app/src/main/java/com/kml/Constants.kt
@@ -0,0 +1,14 @@
+package com.kml
+
+object Constants {
+
+    object Signal {
+        const val EMPTY_POOLS = R.string.no_empty_fields
+        const val TOO_MANY_MINUTES = R.string.too_many_minutes
+        const val VALIDATION_SUCCESSFUL = -1
+    }
+
+    object Strings {
+        const val EMPTY_STRING = ""
+    }
+}
