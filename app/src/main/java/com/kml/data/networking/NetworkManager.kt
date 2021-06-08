55b247b68c77c9a735a1f2b940d3afad3a1a2df1
arix2000 <arix20003@gmail.com>
2021-06-08 20:23:44 +0200

    Migrate to retrofit: refactor

---
+++ app/src/main/java/com/kml/data/networking/NetworkManager.kt
@@ -0,0 +1,33 @@
+package com.kml.data.networking
+
+import com.google.gson.GsonBuilder
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NetworkManager {
+
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .client(getOkHttpClient())
+            .baseUrl(NetworkingConstants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(getGson()))
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build()
+    }
+
+    fun provideApi(retrofit: Retrofit): RestApi {
+        return retrofit.create(RestApi::class.java)
+    }
+
+    private fun getOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+
+    private fun getGson() = GsonBuilder().setLenient().create()
+}
